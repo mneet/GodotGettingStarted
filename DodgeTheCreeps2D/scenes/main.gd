a2f81232e6f6367224003981728dc8734b26b918
Neto <100791626+mneet@users.noreply.github.com>
2023-09-25 15:47:39 -0300

    2D - Display
    Display
    - Start button
    - Score
    - Text message
    
    Everything connected to the main scene

--- DodgeTheCreeps2D/scenes/main.gd
+++ DodgeTheCreeps2D/scenes/main.gd
@@ -5,7 +5,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,11 +16,16 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
+	get_tree().call_group("mob", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	
 
 
 func _on_mob_timer_timeout():
@@ -44,6 +49,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
